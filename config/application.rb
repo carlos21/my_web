8237743d06f4e0b6aa2d9cc0b24aba877faf5e8c
Carlos Duclos <darkzeratul64@gmail.com>
2012-08-27 11:48:47 -0500

    aa

--- config/application.rb
+++ config/application.rb
@@ -58,24 +58,24 @@ module MyWeb
 
     config.autoload_paths += Dir["#{config.root}/models/constants"]
 
-    =begin
-    require 'tlsmail'    
-    Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
-    OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-    ActionMailer::Base.delivery_method = :smtp
-    ActionMailer::Base.perform_deliveries = true
-    ActionMailer::Base.raise_delivery_errors = true
-    ActionMailer::Base.smtp_settings = {
-    :enable_starttls_auto => true,  
-    :address            => 'smtp.gmail.com',
-    :port               => 587,
-    :tls                => true,
-    :domain             => 'gmail.com', #you can also use google.com
-    :authentication     => :plain,
-    :user_name          => 'darkzeratul64.carlos21@gmail.com',
-    :password           => 'GghjFHJgTTytuy67t'
-    }
-    =end
+    
+    #require 'tlsmail'    
+    #Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+    #OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+    #ActionMailer::Base.delivery_method = :smtp
+    #ActionMailer::Base.perform_deliveries = true
+    #ActionMailer::Base.raise_delivery_errors = true
+    #ActionMailer::Base.smtp_settings = {
+    #:enable_starttls_auto => true,  
+    #:address            => 'smtp.gmail.com',
+    #:port               => 587,
+    #:tls                => true,
+    #:domain             => 'gmail.com', #you can also use google.com
+    #:authentication     => :plain,
+    #:user_name          => 'darkzeratul64.carlos21@gmail.com',
+    #:password           => 'GghjFHJgTTytuy67t'
+    #}
+    
 
   end
 end
