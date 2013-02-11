759f49dff1b55405b3822aba0632caaae452f606
Carlos Duclos <darkzeratul64@gmail.com>
2013-02-11 18:05:08 -0500

    aa

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :description, :email, :name, :website, :article_id, :photo_url
+  attr_accessible :description, :email, :name, :website, :photo_url, :article_id
 
   belongs_to :article
   belongs_to :demo
