cb7db98f063b8f656f358e0758d91fb860563470
Carlos Duclos <darkzeratul64@gmail.com>
2013-05-30 06:01:20 +0200

    'lol'

--- app/controllers/car_user_controller.rb
+++ app/controllers/car_user_controller.rb
@@ -1,7 +1,7 @@
 class CarUserController < ApplicationController
 
 	def validate_login
-		user = CarUser.where(username: params['username'], password: ['password']).first
+		user = CarUser.where(username: params['username'], password: params['password']).first
 
 		if user.present?
 			render json: user.to_json
